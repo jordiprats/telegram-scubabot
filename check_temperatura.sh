5bfba47c163ed30a67b00e77ebd0632a9f3f1429
jordiprats <jordi.prats@gmail.com>
2018-01-14 13:53:58 +0100

    adaptat config

--- check_temperatura.sh
+++ check_temperatura.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-function send() 
+function telegramsend() 
 {
         curl -s \
         -X POST \
-        https://api.telegram.org/bot$apiToken/sendMessage \
+        https://api.telegram.org/bot${TOKENBOT}/sendMessage \
         -d text="$1" \
-        -d chat_id=$chatId
+        -d chat_id=$CHATID
 }
 
 DADES_METEOCAT=$(curl 'http://meteo.cat/prediccio/platges/tossa-de-mar-de-la-mar-menuda' -H 'Host: meteo.cat' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.7,ca;q=0.3' --compressed -H 'Referer: http://meteo.cat/prediccio/platges' -H 'Cookie: mapy=41.708829850084335; mapx=2.8837480524089187; mapz=13; __cfduid=d17675a8f06226ee4ddcf89d41ed087d71505470052; _ga=GA1.2.1382788571.1505470053; _gid=GA1.2.2132722468.1516034966' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Cache-Control: max-age=0' 2>/dev/null | grep "dades: \[")
@@ -33,10 +33,28 @@ fi
 
 if [ "${TEMPERATURA_MAX_EXTERIOR}" -ge "${LLINDAR_TEMPERATURA_BUCEIG}" ];
 then
-	send "TOTS cap a l'aigua - temperatura maxima exterior: ${TEMPERATURA_MAX_EXTERIOR} - temperatura maxima de l'aigua: ${TEMPERATURA_MAX_AIGUA}"
+	MESSAGE="TOTS cap a l'aigua - temperatura maxima exterior: ${TEMPERATURA_MAX_EXTERIOR} - temperatura maxima de l'aigua: ${TEMPERATURA_MAX_AIGUA}"
+	SEND=1
 else
+	MESSAGE="no apte per buceig - temperatura maxima exterior: ${TEMPERATURA_MAX_EXTERIOR} - temperatura maxima de l'aigua: ${TEMPERATURA_MAX_AIGUA}"
+	SEND=0
+fi
+
+if [ "$SEND" -eq 1 ];
+then
+	if [ "$DEBUG" -eq 1 ]; then echo "missatge enviat a telegram:"; fi;
+	telegramsend $MESSAGE
+else	
 	if [ "${VERBOSE}" -eq 1 ];
 	then
-		send "no apte per buceig - temperatura maxima exterior: ${TEMPERATURA_MAX_EXTERIOR} - temperatura maxima de l'aigua: ${TEMPERATURA_MAX_AIGUA}"
+		if [ "$DEBUG" -eq 1 ]; then echo "missatge enviat a telegram:"; fi;
+		telegramsend $MESSAGE
+	else
+		if [ "$DEBUG" -eq 1 ]; then echo "NO ENVIAT a telegram:"; fi;
 	fi
+fi
+
+if [ "$DEBUG" -eq 1 ];
+then
+	echo $MESSAGE;
 fi
