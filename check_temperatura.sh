1e6d8775983e3ac3f9c29e5603596fd333c36192
jordiprats <jordi.prats@gmail.com>
2018-01-14 14:11:46 +0100

    funcions cools

--- check_temperatura.sh
+++ check_temperatura.sh
@@ -9,6 +9,30 @@ function telegramsend()
         -d chat_id=$CHATID
 }
 
+function penis()
+{
+	if [ ! -z "$1" ];
+	then
+		echo "   $1   "
+		TAMANY_PENIS=$(echo "   $1   " | wc -c)
+                echo -n " 8"
+                for penis_counter in $(seq 1 $((TAMANY_PENIS-5))); do echo -n =; sleep 0.01; done
+                echo D~
+
+	else
+		echo -n 8
+		for penis_counter in $(seq 1 1$(echo $RANDOM | grep -Eo "^[0-9]")); do echo -n =; sleep 0.01; done
+		echo D~~~
+	fi
+}
+
+function separador()
+{
+	echo -ne ' 8'
+	for i in $(seq 1 $(($(tput cols)-8))); do echo -ne =; sleep 0.005; done
+	echo 'D~~~ '
+}
+
 DADES_METEOCAT=$(curl 'http://meteo.cat/prediccio/platges/tossa-de-mar-de-la-mar-menuda' -H 'Host: meteo.cat' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.7,ca;q=0.3' --compressed -H 'Referer: http://meteo.cat/prediccio/platges' -H 'Cookie: mapy=41.708829850084335; mapx=2.8837480524089187; mapz=13; __cfduid=d17675a8f06226ee4ddcf89d41ed087d71505470052; _ga=GA1.2.1382788571.1505470053; _gid=GA1.2.2132722468.1516034966' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Cache-Control: max-age=0' 2>/dev/null | grep "dades: \[")
 
 TEMPERATURA_MAX_AIGUA=$(echo $DADES_METEOCAT | grep temperatura | sed 's/},{/\n/g' | grep "temperatura_aigua" | awk -F: '{ print $NF }' | sort -n | tail -n1 | cut -f1 -d.)
@@ -29,6 +53,29 @@ else
 		echo "config file missing"
 		exit 1
 	fi
+fi
+
+ABOUTME=`curl -s "https://api.telegram.org/bot${TOKENBOT}/getMe"`
+if [[ "$ABOUTME" =~ \"ok\"\:true\, ]]; 
+then
+	if [[ "$ABOUTME" =~ \"username\"\:\"([^\"]+)\" ]]; 
+	then
+		MYUSERNAME=${BASH_REMATCH[1]}
+	fi
+
+	if [[ "$ABOUTME" =~ \"first_name\"\:\"([^\"]+)\" ]]; 
+	then
+		MYFIRSTNAME=${BASH_REMATCH[1]}
+	fi
+
+	if [[ "$ABOUTME" =~ \"id\"\:([0-9\-]+), ]]; 
+	then
+		BOTID=${BASH_REMATCH[1]};
+	fi
+	if [ "$DEBUG" -eq 1 ]; then penis "@${MYUSERNAME} - ${MYFIRSTNAME} - botid: ${BOTID}"; fi;
+else
+	penis "Error: wrong token";
+	exit 1;
 fi
 
 if [ "${TEMPERATURA_MAX_EXTERIOR}" -ge "${LLINDAR_TEMPERATURA_BUCEIG}" ];
@@ -54,7 +101,4 @@ else
 	fi
 fi
 
-if [ "$DEBUG" -eq 1 ];
-then
-	echo $MESSAGE;
-fi
+if [ "$DEBUG" -eq 1 ]; then penis "$MESSAGE"; fi;
