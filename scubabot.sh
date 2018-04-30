616ed41ae8c87f8c3f10b0f8d4dbbe3f781633ee
jordiprats <jordi.prats@gmail.com>
2018-04-30 02:02:59 +0200

    unificacio mes ruleta de busseig

--- scubabot.sh
+++ scubabot.sh
@@ -4,11 +4,13 @@ export LC_ALL="en_US"
 
 shopt -s nocasematch
 
+function getdadesmeteocat()
+{
+  DADES_METEOCAT=$(curl 'http://meteo.cat/prediccio/platges/tossa-de-mar-de-la-mar-menuda' -H 'Host: meteo.cat' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.7,ca;q=0.3' --compressed -H 'Referer: http://meteo.cat/prediccio/platges' -H 'Cookie: mapy=41.708829850084335; mapx=2.8837480524089187; mapz=13; __cfduid=d17675a8f06226ee4ddcf89d41ed087d71505470052; _ga=GA1.2.1382788571.1505470053; _gid=GA1.2.2132722468.1516034966' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Cache-Control: max-age=0' 2>/dev/null | grep "dades: \[")
+}
+
 function getprevisio()
 {
-  set -x 
-  DADES_METEOCAT=$(curl 'http://meteo.cat/prediccio/platges/tossa-de-mar-de-la-mar-menuda' -H 'Host: meteo.cat' -H 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.7,ca;q=0.3' --compressed -H 'Referer: http://meteo.cat/prediccio/platges' -H 'Cookie: mapy=41.708829850084335; mapx=2.8837480524089187; mapz=13; __cfduid=d17675a8f06226ee4ddcf89d41ed087d71505470052; _ga=GA1.2.1382788571.1505470053; _gid=GA1.2.2132722468.1516034966' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Cache-Control: max-age=0' 2>/dev/null | grep "dades: \[")
-
   DADES_TMP_JSON=$(mktemp /tmp/sexyscubabot.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX)
   echo "{" > $DADES_TMP_JSON
   echo "${DADES_METEOCAT}" >> $DADES_TMP_JSON
@@ -19,6 +21,15 @@ function getprevisio()
   DADES_METEOCAT_JSON_LEAF="$(cat "${DADES_TMP_JSON}" | bash ${BASEDIR}/inc/JSON.sh -l)"
 
   DIES_PREVISIO_DISPONIBLES=$(echo $DADES_METEOCAT | sed 's/}]},/\n/g' | awk -F\" '{ print $4 }' | cut -f1 -dT | sort |uniq)
+
+  if [ "${CRON}" -eq 1 ];
+  then
+    TEMPERATURA_MAX_EXTERIOR=$(echo $DADES_METEOCAT | grep temperatura | sed 's/},{/\n/g' | grep '"temperatura","valor"' | awk -F: '{ print $NF }' | sort -n | tail -n1 | cut -f1 -d.)
+    if [ "${TEMPERATURA_MAX_EXTERIOR}" -ge 20 ];
+    then
+      telegramsend_img "${BASEDIR}/img/sea_calling.jpg"
+    fi
+  fi
 
   DIES_COUNT=0
   for i in ${DIES_PREVISIO_DISPONIBLES};
@@ -124,7 +135,7 @@ function getprevisio()
   # regla del marc
   if (( $(echo "$MAX_ALTURA_ONA < 1.5 " | bc -l) )) && (( $(echo "$MAX_TEMPERATURA >= 20" | bc -l) ));
   then
-    MM_ALGOL_MESSAGE="ja no hi ha *EXCUSES*: algorisme MM(tm) aprova aquesta inmersió"
+    MM_ALGOL_MESSAGE="*JA NO HI HA EXCUSES*: algorisme MM(tm) aprova aquesta inmersió"
   else
     if (( $(echo "$MAX_ALTURA_ONA < 1.5 " | bc -l) ));
     then
@@ -156,11 +167,11 @@ function getprevisio()
                   if [ "$DEBUG" -eq 1 ]; then echo "missatge enviat a telegram:"; fi;
                   telegramsend "$MESSAGE"
           else
-                  if [ "$DEBUG" -eq 1 ]; then echo "NO ENVIAT a telegram:"; fi;
+                  if [ "$DEBUG" -eq 1 ]; then echo "NO ENVIAT a telegram ${CHATID}:"; fi;
           fi
   fi
 
-  if [ "$DEBUG" -eq 1 ]; then echo "$MESSAGE"; fi;
+  if [ "$DEBUG" -eq 1 ]; then echo "chatid: ${CHATID} ${MESSAGE}"; fi;
 
 
   let DIES_COUNT+1
@@ -216,6 +227,18 @@ source $BASEDIR/telegramsend.inc
 source $BASEDIR/douglas.inc
 source $BASEDIR/webcamtossa.inc
 
+CRON=0
+
+while getopts 'ch' OPT;
+do
+  case $OPT in
+    c)  CRON=1;;
+    *)  JELP="yes";;
+  esac
+done
+
+shift $(($OPTIND - 1))
+
 if [ ! -z "$1" ] && [ -f "$1" ];
 then
   . $1 2>/dev/null
@@ -253,7 +276,16 @@ else
   exit 1;
 fi
 
+if [ "$CRON" -eq 1 ];
+then
+  getdadesmeteocat
+  for CHATID in $(find "${BASEDIR}/.db" -type f -exec cat {} \; | sort | uniq);
+  do
+    getprevisio
+  done
 
+  exit 0
+fi
 
 while true; 
 do
@@ -326,6 +358,7 @@ do
 
         if [[ "${TEXT}" =~ "/previsio" ]];
         then
+          getdadesmeteocat
           getprevisio
         fi
 
@@ -362,11 +395,16 @@ do
           telegramsend "https://github.com/jordiprats/telegram-scubabot"
         fi
 
+        if [[ "${TEXT}" =~ "/ruletabussejar" ]];
+        then
+          telegramsend "ha tocat *ANAR A BUSSEJAR*"
+        fi
+
         if [[ "${TEXT}" =~ "/tincansies" ]];
         then
           TEMP_IMG=$(mktemp /tmp/ansiesimg.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX)
 
-          URL_INSTAGRAM="$(echo -e "https://www.instagram.com/cressi1946/\nhttps://www.instagram.com/explore/tags/scubadiving/\nhttps://www.instagram.com/maresjustaddwater/\nhttps://www.instagram.com/suuntodive/\nhttps://www.instagram.com/scubapro/" | shuf | tail -n1)"
+          URL_INSTAGRAM="$(echo -e "ttps://www.instagram.com/paditv/\nhttps://www.instagram.com/cressi1946/\nhttps://www.instagram.com/explore/tags/scubadiving/\nhttps://www.instagram.com/maresjustaddwater/\nhttps://www.instagram.com/suuntodive/\nhttps://www.instagram.com/scubapro/" | shuf | tail -n1)"
 
           wget "$(echo "https:$(curl ${URL_INSTAGRAM} 2>/dev/null | sed 's/},/\n/g' | grep 'edge_liked_by' | awk -F: '$5>50 { print $3,$5 }' | shuf | tail -n1 | cut -f1 -d\")")" -O $TEMP_IMG
 
